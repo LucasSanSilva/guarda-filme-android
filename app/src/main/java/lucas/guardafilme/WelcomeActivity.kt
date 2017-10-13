e7fac2a4d50f17040ee511d0abaf07ee94fb1ca3
Lucas Santos Silva <lucas.santos3@gmail.com>
2017-10-13 10:10:08 -0300

    Adiciona botão para logout

--- app/src/main/java/lucas/guardafilme/WelcomeActivity.kt
+++ app/src/main/java/lucas/guardafilme/WelcomeActivity.kt
@@ -6,7 +6,10 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
+import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
 import com.firebase.ui.database.FirebaseRecyclerAdapter
 import com.google.firebase.auth.FirebaseAuth
@@ -81,6 +84,25 @@ class WelcomeActivity: AppCompatActivity() {
             }
             moviesRecyclerView.adapter = mAdapter
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.logout_item) {
+            AuthUI.getInstance()
+                    .signOut(this)
+                    .addOnCompleteListener { task ->
+                        startActivity(Intent(this, MainActivity::class.java))
+                    }
+            finish()
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 
     companion object {
