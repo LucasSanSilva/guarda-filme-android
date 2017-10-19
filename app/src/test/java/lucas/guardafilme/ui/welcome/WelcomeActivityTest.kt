80520251d13d50784c07d3c6023566535cec0afd
Lucas Santos Silva <lucas.santos3@gmail.com>
2017-10-19 08:46:56 -0200

    Adiciona Robolectric

---
+++ app/src/test/java/lucas/guardafilme/ui/welcome/WelcomeActivityTest.kt
@@ -0,0 +1,29 @@
+package lucas.guardafilme.ui.welcome
+
+import android.content.Intent
+import android.support.design.widget.FloatingActionButton
+import kotlinx.android.synthetic.main.activity_welcome.view.*
+import lucas.guardafilme.R
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.Robolectric
+import org.robolectric.RobolectricTestRunner
+
+/**
+ * Created by lucassantos on 19/10/17.
+ */
+@RunWith(RobolectricTestRunner::class)
+class WelcomeActivityTest {
+
+    @Test
+    fun clickAdd_shouldStartSearchMovieActivity() {
+//        val intent = Intent()
+//        intent.putExtra(WelcomeActivity.USER_ID_EXTRA, "teste")
+//        val activity: WelcomeActivity = Robolectric
+//                .buildActivity(WelcomeActivity::class.java, intent)
+//                .create()
+//                .get()
+//        activity.findViewById<FloatingActionButton>(R.id.fab).performClick()
+    }
+
+}
