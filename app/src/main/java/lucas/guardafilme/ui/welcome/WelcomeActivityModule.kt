025b919062ab04b4c24061346c353749f66d129e
Lucas Santos Silva <lucas.santos3@gmail.com>
2017-10-23 06:21:25 -0200

    Corrige WelcomeActivityModule

--- app/src/main/java/lucas/guardafilme/ui/welcome/WelcomeActivityModule.kt
+++ app/src/main/java/lucas/guardafilme/ui/welcome/WelcomeActivityModule.kt
@@ -1,6 +1,7 @@
 package lucas.guardafilme.ui.welcome
 
 import dagger.Binds
+import dagger.Module
 import lucas.guardafilme.data.AuthProvider
 import lucas.guardafilme.data.GFAuthProvider
 import lucas.guardafilme.di.ActivityScoped
@@ -8,6 +9,7 @@ import lucas.guardafilme.di.ActivityScoped
 /**
  * Created by lucassantos on 21/10/17.
  */
+@Module
 abstract class WelcomeActivityModule {
     @ActivityScoped
     @Binds abstract fun authProvider(authProvider: GFAuthProvider): AuthProvider
